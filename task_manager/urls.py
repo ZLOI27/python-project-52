c7ba8f1c10e24c47e46a164a4820bf968ed7bfd7
Kirill Zlobinov <zlobinovk@yandex.ru>
2026-07-12 16:51:10 +0300

    add: statuses update and delete

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("users/", include("users.urls")),
-    # path("statuses/", include("statuses.urls")),
+    path("statuses/", include("statuses.urls")),
     # path("tasks/", include("tasks.urls")),
 ]
